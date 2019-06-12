56c20a060e1ae3bd16ca40edee8acbf1fde34661
Selena <espinalselena12m@gmail.com>
2019-06-11 18:59:08 -0600

    Obtiene la coleccion de recetas

---
+++ Ex10601199601902/routes/api/api.js
@@ -0,0 +1,10 @@
+var express= require('express');
+var router = express.Router();
+
+var recetaRoutes = require('./recetas');
+
+router.use('/recetas', recetaRoutes);
+
+
+
+module.exports = router;
---
+++ Ex10601199601902/routes/api/recetas.js
@@ -0,0 +1,42 @@
+var express = require('expess');
+var router = express.Router();
+
+var uuid = require('uuid/v4');
+
+var recetaCollection =[];
+
+var recetaStruct = {
+    id:0,
+    nombre:'',
+    correo:'',
+    telefono:'',
+    nota:''
+}
+
+recetaCollection.push(
+    object.assign(
+        {},
+        recetaStruct,
+        {
+            id:uuid(),
+            receta:'Medica',
+            precio:'200',
+            tipo:'nutrición',
+            observacion:'cualquiera',
+            estado: 'cuanquiera'
+
+        }
+
+        )
+
+);
+
+
+
+router.get('/',(req,res,next)=>{
+  
+   res.status(200).json(recetaCollection);
+
+});
+
+
